6ecb629984309f7188253e07ab673d8a799caa8e
Ahmed <ahmed@Ahmeds-MacBook-Pro.local>
2022-02-13 23:52:03 -0800

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className='header'>
+      <h2 className='header__title'>Meme Generator</h2>
+      <h4 className='header__project'>React Course - Simple Project</h4>
+    </header>
+  );
+}
